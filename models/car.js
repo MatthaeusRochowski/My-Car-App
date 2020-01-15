8ab79d9f075b541a43bdf849919ea6563a87626b
Malte Schenkling <schenkling@mediamarktsaturn.com>
2020-01-15 16:06:47 +0100

    added updated car model

--- models/car.js
+++ models/car.js
@@ -9,15 +9,47 @@ const carSchema = new Schema({
   tsn:                    { type: String }, //Typschlüsselnummer (AAB, first 3 digits)
   kraftstoff:             { type: String }, //Benzin
   leistung_ps:            { type: Number }, //140
+  erstzulassung_monat:    { type: Number }, //1 (Januar)
+  erstzulassung_jahr:     { type: Number }, //2020
   kaufpreis:              { type: Number }, //15000
   kilometerstand_kauf:    { type: Number }, //0
-  kilometerstand_aktuell: { type: Number }, //12000
+  kilometerstand_aktuell: { type: Number }, //12000 (updated by most recent kilometerstand_ende)
 
-  eigner_ref:        { type: Schema.Types.ObjectId, ref: 'User' }, //check with anastasia
-  logbuch_ref:      [{ type: Schema.Types.ObjectId, ref: 'Logbuch' }],
-  werkstatt_ref:    [{ type: Schema.Types.ObjectId, ref: 'Werkstatt' }],
-  versicherung_ref: [{ type: Schema.Types.ObjectId, ref: 'Versicherung' }]
-  })
+  kfz_steuer:             { type: Number }, //150
+  
+  fahrtenbuch: [{
+    strecke_km:           { type: Number }, //50 (will be calculated kilometerstandende - kilometerstand_start)
+    startort:             { type: String }, //zuhause
+    zielort:              { type: String }, //arbeit
+    kilometerstand_start: { type: Number }, //12000 (defaulted to kilometerstand_aktuell)
+    kilometerstand_ende:  { type: Number }  //12050
+  }],
+
+  tankbuch: [{
+    kilometerstand: { type: Number }, //12025
+    liter:          { type: Number }, //35
+    literpreis:     { type: Number }, //1.4
+    gesamtpreis:    { type: Number }  //49 (liter * literpreis)
+  }], 
+
+  werkstattbuch: [{
+    kilometerstand:  { type: Number }, //12040
+    reparaturposten: { type: String }, //Keilriemen
+    reparaturpreis:  { type: Number }, //230
+    rechnungs_url:   { type: String }  //http://cloundinary/user/....
+  }],
+
+  versicherungsbuch: [{
+    name:                     { type: String }, //HanseMerkur
+    typ:                      { type: String }, //VK, TK, HF
+    abschluss_jahr:           { type: Number }, //2020 (initial insurance abschluss_monat = erstzulassung_monat)
+    abschluss_kilometerstand: { type: Number }, //12000
+    geschätzte_laufleistung:  { type: Number }, //10000
+    versicherungskosten_jahr: { type: Number }  //600
+  }], 
+
+  eigner_ref:        { type: Schema.Types.ObjectId, ref: 'User' }, //check with anastasia (add schadensfreiheitsklasse)
+})
 
   const Car = mongoose.model("Car", carSchema);
 
