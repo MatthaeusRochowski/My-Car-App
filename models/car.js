2fb1341350016b874feb0ddfcacda9550c10f838
Malte Schenkling <schenkling@mediamarktsaturn.com>
2020-01-15 16:13:51 +0100

    added updated car model 2

--- models/car.js
+++ models/car.js
@@ -15,7 +15,7 @@ const carSchema = new Schema({
   kilometerstand_kauf:    { type: Number }, //0
   kilometerstand_aktuell: { type: Number }, //12000 (updated by most recent kilometerstand_ende)
 
-  kfz_steuer:             { type: Number }, //150
+  kfz_steuer: { type: Number }, //150
   
   fahrtenbuch: [{
     strecke_km:           { type: Number }, //50 (will be calculated kilometerstandende - kilometerstand_start)
@@ -45,10 +45,11 @@ const carSchema = new Schema({
     abschluss_jahr:           { type: Number }, //2020 (initial insurance abschluss_monat = erstzulassung_monat)
     abschluss_kilometerstand: { type: Number }, //12000
     geschätzte_laufleistung:  { type: Number }, //10000
-    versicherungskosten_jahr: { type: Number }  //600
+    versicherungskosten_jahr: { type: Number }, //600
+    schadensfreiheitsklasse:  { type: Number }  //18 (belongs to user and insurer)
   }], 
 
-  eigner_ref:        { type: Schema.Types.ObjectId, ref: 'User' }, //check with anastasia (add schadensfreiheitsklasse)
+  eigner_ref: { type: Schema.Types.ObjectId, ref: 'User' }, //check with anastasia
 })
 
   const Car = mongoose.model("Car", carSchema);
